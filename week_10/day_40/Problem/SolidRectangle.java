3a70508764907cac5063b3646a98220755aafd14
Ankit Parida <ankitparida386@gmail.com>
2025-09-12 10:44:16 +0530

    Adding the work of day_40 of DSOOPS

---
+++ week_10/day_40/Problem/SolidRectangle.java
@@ -0,0 +1,12 @@
+package week_10.day_40.Problem;
+
+public class SolidRectangle {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
