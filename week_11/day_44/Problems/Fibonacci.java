91d7b51477e0e599891feb730eab89e263535d51
Ankit Parida <ankitparida386@gmail.com>
2025-09-21 23:09:49 +0530

    Adding the day-44 to the java classes

---
+++ week_11/day_44/Problems/Fibonacci.java
@@ -0,0 +1,22 @@
+package week_11.day_44.Problems;
+// import java.util.*;
+import java.util.Scanner;
+public class Fibonacci {
+    public static int fibonacci(int n){
+            if(n==0){
+                return 0;
+            }
+            else if(n==1){
+                return 1;
+            }
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    public static void main(String[] args) {
+       int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        n=sc.nextInt();
+        System.out.print("The fibonacci("+n+") is "+" "+fibonacci(n));
+        sc.close();
+    }
+}
---
+++ week_11/day_44/Problems/Sum.java
@@ -0,0 +1,22 @@
+package week_11.day_44.Problems;
+
+import java.util.Scanner;
+public class Sum {
+    public static int sum(int n){
+            if(n==0){
+               return 0;
+            }
+           else if(n==1){
+            return 1;
+           }
+           return n+sum(n-1);
+        }
+    public static void main(String[] args) {
+       int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        n=sc.nextInt();
+        System.out.print("The sum of the numbers upto the "+n+" "+"is"+" "+sum(n));
+        sc.close();
+    }
+}
